06b0b923421791e5ec14118465c2fdfaae1915e2
Artem Klots <artem_klots@mail.ua>
2018-12-10 21:23:46 +0200

    Add forgotten @Repository annotation

--- src/main/java/com/travelBill/api/user/UserRepository.java
+++ src/main/java/com/travelBill/api/user/UserRepository.java
@@ -2,6 +2,8 @@ package com.travelBill.api.user;
 
 import com.travelBill.api.core.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }

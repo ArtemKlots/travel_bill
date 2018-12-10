b4c980e9f9e9045748da0cc47c4ba6ff6831dfc6
Artem Klots <artem_klots@mail.ua>
2018-12-10 21:24:27 +0200

    Add forgotten @Service annotations

--- src/main/java/com/travelBill/api/user/UserService.java
+++ src/main/java/com/travelBill/api/user/UserService.java
@@ -1,9 +1,21 @@
 package com.travelBill.api.user;
 
+import com.travelBill.api.core.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User byTelegramId(Integer id) {
+        return userRepository.findUserByTelegramId(id);
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
     }
 }

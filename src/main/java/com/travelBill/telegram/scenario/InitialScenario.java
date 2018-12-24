afc1a59da27a7c502e716e7628f9377016fb4f2a
Artem Klots <artem_klots@mail.ua>
2018-12-24 22:04:01 +0200

    Refactor ShowEventsListScenario and InitialScenario

--- src/main/java/com/travelBill/telegram/scenario/InitialScenario.java
+++ src/main/java/com/travelBill/telegram/scenario/InitialScenario.java
@@ -3,6 +3,7 @@ package com.travelBill.telegram.scenario;
 import com.travelBill.telegram.scenario.common.Scenario;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
@@ -19,11 +20,15 @@ public class InitialScenario implements Scenario {
     @Override
     public SendMessage createMessage() {
         long chat_id = update.getMessage().getChatId();
+        ReplyKeyboard markup = createMarkup();
 
-        SendMessage message = new SendMessage()
+        return new SendMessage()
                 .setChatId(chat_id)
-                .setText("What would you like to do?");
+                .setText("What would you like to do?")
+                .setReplyMarkup(markup);
+    }
 
+    private ReplyKeyboard createMarkup() {
         ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup();
         List<KeyboardRow> keyboard = new ArrayList<>();
 
@@ -37,9 +42,6 @@ public class InitialScenario implements Scenario {
         keyboard.add(currentEventRow);
 
         markup.setKeyboard(keyboard);
-
-        message.setReplyMarkup(markup);
-
-        return message;
+        return markup;
     }
 }

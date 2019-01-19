df68e14dc36f87599f4f9e8e9a2ee35d800e98ab
Artem Klots <artem_klots@mail.ua>
2019-01-19 12:24:50 +0200

    Fix an error when event Update was not provided

--- src/main/java/com/travelBill/telegram/scenario/event/EventContext.java
+++ src/main/java/com/travelBill/telegram/scenario/event/EventContext.java
@@ -3,11 +3,9 @@ package com.travelBill.telegram.scenario.event;
 import com.travelBill.api.core.event.EventService;
 import com.travelBill.api.core.user.User;
 import com.travelBill.telegram.scenario.Context;
-import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class EventContext extends Context {
     public EventService eventService;
-    public Update update;
     public User currentUser;
 
 }

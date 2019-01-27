2cf303339f19e304e4b9e12ce807f86c735eca14
Artem Klots <artem_klots@mail.ua>
2019-01-27 18:34:55 +0200

    Add flyway with migration to setup database schema. Add application configuration

---
+++ src/main/resources/db/migration/V201901271530__init_schema.sql
@@ -0,0 +1,50 @@
+CREATE TABLE `bill` (
+  `id` bigint(20) NOT NULL,
+  `amount` double (10, 2) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `event_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `currency` varchar(255) DEFAULT NULL,
+  `purpose` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `event` (
+  `id` bigint(20) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `owner_id` bigint(20) DEFAULT NULL,
+  `telegram_chat_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `event_user` (
+  `event_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `telegram_id` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `current_event_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE bill ADD FOREIGN KEY (event_id) REFERENCES event(event_id);
+ALTER TABLE bill ADD FOREIGN KEY (user_id) REFERENCES user(user_id);
+ALTER TABLE event ADD FOREIGN KEY (owner_id) REFERENCES user(owner_id);
+ALTER TABLE event_user ADD FOREIGN KEY (user_id) REFERENCES user(user_id);
+ALTER TABLE event_user ADD FOREIGN KEY (event_id) REFERENCES event(event_id);
+ALTER TABLE user ADD FOREIGN KEY (current_event_id) REFERENCES event(event_id);

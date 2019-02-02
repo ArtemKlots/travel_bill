ad8d277145a055317d2ae384f32cacd574e44f33
Artem Klots <artem_klots@mail.ua>
2019-02-02 22:11:10 +0200

    Divide BillService to interface and implementation

--- src/main/java/com/travelBill/api/core/bill/BillService.java
+++ src/main/java/com/travelBill/api/core/bill/BillService.java
@@ -1,25 +1,9 @@
 package com.travelBill.api.core.bill;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class BillService {
+public interface BillService {
+    List<Bill> getAll();
 
-    private final BillRepository billRepository;
-
-    @Autowired
-    public BillService(BillRepository BillRepository) {
-        this.billRepository = BillRepository;
-    }
-
-    public List<Bill> getAll() {
-        return billRepository.findAll();
-    }
-
-    public Bill save(Bill bill) {
-        return billRepository.save(bill);
-    }
+    Bill save(Bill bill);
 }
---
+++ src/main/java/com/travelBill/api/core/bill/BillServiceImpl.java
@@ -0,0 +1,26 @@
+package com.travelBill.api.core.bill;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BillServiceImpl implements BillService {
+
+    private final BillRepository billRepository;
+
+    @Autowired
+    public BillServiceImpl(BillRepository BillRepository) {
+        this.billRepository = BillRepository;
+    }
+
+    public List<Bill> getAll() {
+        return billRepository.findAll();
+    }
+
+    public Bill save(Bill bill) {
+        return billRepository.save(bill);
+    }
+
+}

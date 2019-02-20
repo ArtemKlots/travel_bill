1302bea6fb2d1e30e8258193d6d921aea335fe88
Artem Klots <artem_klots@mail.ua>
2019-02-21 00:14:48 +0200

    Add cron tasks to support application work on the heroku (it shutdown application after 1 hour of incativity), and use healthcheck's webhook

---
+++ src/main/java/com/travelBill/heroku/LifecycleSupporter.java
@@ -0,0 +1,45 @@
+package com.travelBill.heroku;
+
+import com.travelBill.config.ApplicationConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+@Service
+public class LifecycleSupporter {
+    private final ApplicationConfiguration applicationConfiguration;
+
+    @Autowired
+    public LifecycleSupporter(ApplicationConfiguration applicationConfiguration) {
+        this.applicationConfiguration = applicationConfiguration;
+    }
+
+    @Scheduled(fixedRate = 40 * 60 * 1000)
+    public void resetLifecycle() {
+        try {
+            URL url = new URL(applicationConfiguration.getLifecycleUrl());
+            URLConnection connection = url.openConnection();
+            InputStream is = connection.getInputStream();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(fixedRate = 90 * 60 * 1000)
+    public void reportHealth() {
+        try {
+            URL url = new URL(applicationConfiguration.getHeathCheckUrl());
+            URLConnection connection = url.openConnection();
+            InputStream is = connection.getInputStream();
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}

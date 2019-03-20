7b7d100502bf8af1b46a7f774acc66687aae9c24
Artem Klots <artem_klots@mail.ua>
2019-03-20 22:08:36 +0200

    WIP implement ShowLastTransactionsScenario
    Now returns all transaction

--- src/main/java/com/travelBill/api/core/bill/BillRepository.java
+++ src/main/java/com/travelBill/api/core/bill/BillRepository.java
@@ -2,5 +2,8 @@ package com.travelBill.api.core.bill;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BillRepository extends JpaRepository<Bill, Long> {
+    List<Bill> selectByUserIdOrderByCreatedAt(Long id);
 }

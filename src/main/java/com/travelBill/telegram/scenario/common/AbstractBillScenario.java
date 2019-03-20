ad46f13a450b84ac33061b1b3b950ab11a8846a5
Artem Klots <artem_klots@mail.ua>
2019-03-20 22:07:54 +0200

    Move AbstractBillScenario to upper level

--- src/main/java/com/travelBill/telegram/scenario/common/AbstractBillScenario.java
+++ src/main/java/com/travelBill/telegram/scenario/common/AbstractBillScenario.java
@@ -1,13 +1,13 @@
-package com.travelBill.telegram.scenario.group.bill;
+package com.travelBill.telegram.scenario.common;
 
 import com.travelBill.telegram.scenario.common.context.BillContext;
 import com.travelBill.telegram.scenario.common.scenario.Scenario;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 public abstract class AbstractBillScenario implements Scenario {
-    BillContext billContext;
+    public BillContext billContext;
 
-    AbstractBillScenario(BillContext billContext) {
+    public AbstractBillScenario(BillContext billContext) {
         this.billContext = billContext;
     }
 

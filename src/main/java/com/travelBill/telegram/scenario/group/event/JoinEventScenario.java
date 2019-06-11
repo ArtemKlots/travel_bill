f977634b38ef9ec8a2ca251b8135cdbe634a9136
Artem Klots <artem_klots@mail.ua>
2019-06-11 22:38:27 +0300

    Prevent user from multiply joining the chat

--- src/main/java/com/travelBill/telegram/scenario/group/event/JoinEventScenario.java
+++ src/main/java/com/travelBill/telegram/scenario/group/event/JoinEventScenario.java
@@ -10,29 +10,42 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JoinEventScenario extends AbstractEventScenario {
     public JoinEventScenario(EventContext eventContext) {
         super(eventContext);
     }
 
+    // TODO: 11.06.19 Refactor it
     public SendMessage createMessage() {
         Event event = eventContext.eventService.findByTelegramChatId(eventContext.getChatId());
 
-        eventContext.eventService.addMember(event, eventContext.currentUser);
+        if (isNewMember(event, eventContext)) {
 
-        String responseMessage = String.format(
-                "Hello %s %s! Now I know that you are here and can receive your contributions :) \n" +
-                        "Use the following pattern to make a contribution: <how much> <currency> <purpose> (spaces are important!)\n" +
-                        "For example: 10 points to gryffindor",
-                eventContext.currentUser.getFirstName(),
-                eventContext.currentUser.getLastName()
-        );
+            eventContext.eventService.addMember(event, eventContext.currentUser);
 
-        return new SendMessage()
-                .setChatId(eventContext.getChatId())
-                .setText(responseMessage)
-                .setReplyMarkup(createMarkup());
+            String responseMessage = String.format(
+                    "Hello %s %s! Now I know that you are here and can receive your contributions :) \n\n" +
+                            "Use the following pattern to make a contribution: <how much> <currency> <purpose> (spaces are important!)\n\n" +
+                            "For example: 10 points to gryffindor",
+                    eventContext.currentUser.getFirstName(),
+                    eventContext.currentUser.getLastName()
+            );
+
+            return new SendMessage()
+                    .setChatId(eventContext.getChatId())
+                    .setText(responseMessage)
+                    .setReplyMarkup(createMarkup());
+        } else {
+            String message = String.format("Hmm... It seems I saw you in this chat. You are %s %s, right?",
+                    eventContext.currentUser.getFirstName(),
+                    eventContext.currentUser.getLastName());
+
+            return new SendMessage()
+                    .setChatId(eventContext.getChatId())
+                    .setText(message);
+        }
     }
 
     private ReplyKeyboard createMarkup() {
@@ -47,5 +60,14 @@ public class JoinEventScenario extends AbstractEventScenario {
         markup.setKeyboard(keyboard);
         markup.setResizeKeyboard(true);
         return markup;
+    }
+
+    private boolean isNewMember(Event event, EventContext eventContext) {
+        List matchedUser = event.getMembers()
+                .stream()
+                .filter(u -> u.getId().equals(eventContext.currentUser.getId()))
+                .collect(Collectors.toList());
+
+        return matchedUser.size() == 0;
     }
 }

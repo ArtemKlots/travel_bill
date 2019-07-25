b2655bc1ad3bc5f7f65a3c36c5000eb373d816f6
Artem Klots <artem_klots@mail.ua>
2019-07-26 00:23:28 +0300

    Delete bills list after delete flow was finished

--- src/main/java/com/travelBill/telegram/scenario/group/bill/DeleteBillScenario.java
+++ src/main/java/com/travelBill/telegram/scenario/group/bill/DeleteBillScenario.java
@@ -1,10 +1,11 @@
 package com.travelBill.telegram.scenario.group.bill;
 
 import com.travelBill.api.core.bill.Bill;
-import com.travelBill.telegram.TelegramUpdateUtils;
 import com.travelBill.telegram.scenario.common.AbstractBillScenario;
 import com.travelBill.telegram.scenario.common.context.BillContext;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import java.io.IOException;
 
 public class DeleteBillScenario extends AbstractBillScenario {
 
@@ -16,7 +17,7 @@ public class DeleteBillScenario extends AbstractBillScenario {
 
     @Override
     public SendMessage createMessage() {
-        Long telegramChatId = new TelegramUpdateUtils().getChatId(billContext.update);
+        Long telegramChatId = billContext.getChatId();
         String stringBillId = billContext.update.getCallbackQuery().getData().split("_")[BILL_ID_INDEX_IN_CALLBACK];
         Long billId = Long.parseLong(stringBillId);
         Bill billToRemove = billContext.billService.findById(billId);
@@ -26,7 +27,7 @@ public class DeleteBillScenario extends AbstractBillScenario {
         if (billToRemove.getUser().getId().equals(billContext.currentUser.getId())) {
             try {
                 billContext.billService.deleteById(billId);
-                resultText = String.format("*%s %s %s* has been removed successfully by *%s*",
+                resultText = String.format("*%s %s %s* has been successfully removed by *%s*",
                         billToRemove.getAmount(), billToRemove.getCurrency(), billToRemove.getPurpose(), billContext.currentUser.getFullName());
             } catch (Exception e) {
                 resultText = "Something went wrong :( Probably bill was not deleted";
@@ -35,9 +36,22 @@ public class DeleteBillScenario extends AbstractBillScenario {
             resultText = String.format("Hmm.. %s, it seems it is not your bill", billContext.currentUser.getFullName());
         }
 
+        deleteDebtList(billContext);
+
         return new SendMessage()
                 .setChatId(telegramChatId)
                 .setText(resultText)
                 .setParseMode("Markdown");
+    }
+
+    private void deleteDebtList(BillContext billContext) {
+        Long telegramChatId = billContext.getChatId();
+        Integer messageId = billContext.update.getCallbackQuery().getMessage().getMessageId();
+        try {
+            billContext.botApi.deleteMessage(telegramChatId, messageId);
+        } catch (IOException e) {
+            System.out.println("Error during message deleting");
+            e.printStackTrace();
+        }
     }
 }

60f3e98180c3ca8b72c474b0502c7672cec97ca4
Artem Klots <artem.klots.dev@gmail.com>
2020-08-13 21:17:34 +0300

    Cover DebtRepository with tests

--- src/main/java/com/travelBill/api/core/debt/DebtRepository.java
+++ src/main/java/com/travelBill/api/core/debt/DebtRepository.java
@@ -6,18 +6,17 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-//TODO Cover with tests
 public interface DebtRepository extends JpaRepository<Debt, Long> {
     /**
      * Select and count all debts for user (grouped by currency and > 0)
      * The main query counts "How much do I owe", and the sub query counts "How mush somebody owed to me"
      */
     @Query(value = "SELECT currency, debtor_id as debtorId, user.first_name as userFirstName, user.last_name as userLastName, payer_id as payerId, " +
-            "(SELECT sum(debt.amount) + IFNULL(sum(debt_subquery.amount) * -1, 0) FROM travel_bill.debt debt_subquery" +
+            "(SELECT sum(debt.amount) + IFNULL(sum(debt_subquery.amount) * -1, 0) FROM debt debt_subquery" +
             " where debt_subquery.payer_id = debt_subquery.payer_id " +
             "   and debt_subquery.debtor_id = :user_id " +
             "   and debt_subquery.currency = debt.currency) as amount " +
-            " FROM travel_bill.debt debt" +
+            " FROM debt" +
             " INNER JOIN user ON debt.debtor_id = user.id " +
             " WHERE payer_id = :user_id" +
             " GROUP BY debtor_id, currency",

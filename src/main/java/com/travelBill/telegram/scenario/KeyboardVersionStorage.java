ee893c92330e916ec1f3abbd921313d0ed80de7f
Artem Klots <artem.klots.dev@gmail.com>
2020-10-01 00:53:07 +0300

    Update keyboard time to current

--- src/main/java/com/travelBill/telegram/scenario/KeyboardVersionStorage.java
+++ src/main/java/com/travelBill/telegram/scenario/KeyboardVersionStorage.java
@@ -7,10 +7,10 @@ import java.time.LocalDateTime;
 @Service
 public class KeyboardVersionStorage {
     public LocalDateTime getPrivateKeyboardReleaseDate() {
-        return LocalDateTime.of(2020, 10, 1, 12, 0);
+        return LocalDateTime.of(2020, 9, 30, 23, 59);
     }
 
     public LocalDateTime getGroupKeyboardReleaseDate() {
-        return LocalDateTime.of(2020, 10, 1, 12, 0);
+        return LocalDateTime.of(2020, 9, 30, 23, 59);
     }
 }
